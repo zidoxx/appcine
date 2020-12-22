52e903760b97696222c31a3352d756eb210034b6
Esteban Castaño Rivera <estebancastriver@gmail.com>
2020-12-22 14:05:10 -0500

    Conexión mongodb

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/appcine',
+    // url: 'mongodb://localhost:27017/appcine',
+    url:
+        "mongodb+srv://seminario:6SaPbfsGfKpw6amQ@cluster0.xdi4o.mongodb.net/appcine?retryWrites=true&w=majority",
     options: {
-        
         useCreateIndex: true,
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+        useUnifiedTopology: true,
+    },
 };
